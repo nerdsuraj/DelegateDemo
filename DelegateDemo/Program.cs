37e1b605cd5a915be7cf17665c2d076a67306c58
suraj <soorajkrpandit@gmail.com>
2022-05-10 10:44:45 +0530

    Add project files.

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    internal class Program
+    {
+        public delegate int operation(int x, int y);
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static int Addition123(int a, int b, int c)
+        {
+            return a + b;
+        }
+
+        public static void Main(String [] args)
+        {
+            Console.WriteLine("Delegate Demo");
+
+            //operation obj = new operation(Addition);
+
+            //Console.WriteLine("Addition is = {0}",obj(23,27));
+
+            MulticastDelegate1.ImplementDelegate();
+
+            Console.ReadKey();
+        }
+        
+
+
+    }
+}
+
---
+++ EventsDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //add += new DelEventHandler(India);
+            //add += new DelEventHandler(USA);
+            //add += new DelEventHandler(England);
+            //add.Invoke();
+            Console.WriteLine("************Event Handling****************");
+            EventHandling.ImplementEvent();
+            Console.ReadKey();
+        }
+    }
+}
